a4cf1eec73153e2c1b911f03071bded0bc69cdf7
jishasandeep <jishasandeep@gmail.com>
2025-05-13 14:56:30 +0200

    basic authentication changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation ("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.projectlombok:lombok:1.18.26")
 	//implementation("org.springframework.boot", "spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
 	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
 	//developmentOnly("org.springframework.boot:spring-boot-devtools")
 	//runtimeOnly ("com.h2database:h2")
 	runtimeOnly ("com.microsoft.sqlserver:mssql-jdbc")
